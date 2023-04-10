b07b40887c4fd1640e55d4b8e66e4651cb6c15bc
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2023-04-10 09:37:42 -0300

    Add files via upload

---
+++ src/Login.jsx
@@ -0,0 +1,15 @@
+function Login(){
+    return (
+    <div class="login">
+      <h1>Faça seu cadasto</h1>
+      {/* <h2>Login</h2> */}
+      <p>Coloque seu email abaixo</p>
+      <input type="email" name="email" id="" placeholder="email"></input>
+      <p>Coloque sua senha abaixo</p>
+      <input type="password" name="senha" id="senha" placeholder="senha"></input>
+      <button value="Cadastar">Cadastrar</button>
+    </div>
+    )
+   }
+   export default Login
+
